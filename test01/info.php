edd71dd4bb723dfff931d48e819481f5739f17c9
zhangyonglei <zhangyonglei@aragoncs.com>
2014-11-29 10:09:25 +0800

    REV: remove password showing.

--- test01/info.php
+++ test01/info.php
@@ -1,7 +1,6 @@
 <?php
 echo "<br>DB_NAME: " . getenv('DB_NAME');
 echo "<br>DB_USER: " . getenv('DB_USER');
-echo "<br>DB_PASSWORD: " . getenv('DB_PASSWORD');
 echo "<br>HTTP_HOST: " . getenv('HTTP_HOST');
 echo "<br>=========================<br>";
 echo phpinfo();
